ce59426f8eb192e87e1652213c8ce32d92348110
WitFelm <jacobwitflem@gmail.com>
2020-10-17 14:19:38 +0300

    dfsdf

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import os
 from os import environ
 from pyrogram import Client
 
-api_id = int(environ["API_ID"])
-api_hash = environ["API_HASH"]
+from boto.s3.connection import S3Connection
+
+
+api_id = environ.get('API_ID')
+api_hash = environ.get("API_HASH")
 info = "Greetings from **Heroku**!"
 
-app = Client(":memory:", api_id, api_hash)
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
-print(info)
+print(api_id + "" +api_hash)
+print(s3)
+# app = Client(":memory:", api_id, api_hash)
 
-@app.on_message()
-def work(client, message):
-    app.send_message(message.chat.id, info)
+# print(info)
 
-app.run()
+# @app.on_message()
+# def work(client, message):
+#     app.send_message(message.chat.id, info)
+
+# app.run()
