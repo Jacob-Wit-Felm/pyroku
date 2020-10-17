d2f86ff0e62368bbeee481afd2d89cfeed142e67
WitFelm <jacobwitflem@gmail.com>
2020-10-17 16:45:27 +0300

    remove tests

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ import os
 from os import environ
 from pyrogram import Client
 
-from boto.s3.connection import S3Connection
-
-
 api_id = environ.get('API_ID')
 api_hash = environ.get("API_HASH")
 info = "Greetings from **Heroku**!"
 
-s3 = S3Connection(os.environ.get('S3_KEY'), os.environ.get('S3_SECRET'))
-
 print(api_id)
 print(api_hash)
-print(s3)
 
 app = Client(":memory:", api_id, api_hash)
 
